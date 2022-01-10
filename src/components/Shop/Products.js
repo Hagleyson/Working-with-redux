babb942a82bfccf14bf52053cf30da523009c71a
hagleyson <hagleyson@gmail.com>
2022-01-10 13:16:42 -0300

    reviewing redux

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,36 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "my first book",
+    description: "the first book i ever wrote",
+  },
+  {
+    id: "p2",
+    price: 7,
+    title: "my second book",
+    description: "the second book i ever wrote",
+  },
+];
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((products) => (
+          <ProductItem
+            key={products.id}
+            id={products.id}
+            title={products.title}
+            price={products.price}
+            description={products.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
